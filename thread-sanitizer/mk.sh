678969e97391dd60ee4806050010d8ba979059b9
Xinan Xu <xx2153@columbia.edu>
2013-09-17 23:52:37 -0400

    making script for tsan v1

--- thread-sanitizer/mk.sh
+++ thread-sanitizer/mk.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-svn checkout http://thread-sanitizer.googlecode.com/svn/trunk/ src
+rm -rf drt get_and_build_tsan.sh
+wget http://data-race-test.googlecode.com/svn/trunk/tsan/get_and_build_tsan.sh
+chmod +x ./get_and_build_tsan.sh
+patch -p0 < make_script.patch
+./get_and_build_tsan.sh `pwd`/tsan_inst_tmp
