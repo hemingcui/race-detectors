ab7836eea786c6b242cb97fa3a5aab2775363fee
Xinan Xu <xx2153@columbia.edu>
2013-09-18 16:04:59 -0400

    setting up relay, some script not portable...

--- mk-all.sh
+++ mk-all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 pushd thread-sanitizer
+./mk.sh
 popd thread-sanitizer
 echo "done!"
---
+++ relay/mk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+sudo apt-get install ocaml
+pushd /usr/lib/ocaml
+sudo ln -s libcamlstr.a libstr.a
+popd
+rm relay-radar.0.10.03.tar.gz
+wget http://cseweb.ucsd.edu/~jvoung/code/relay-radar.0.10.03.tar.gz
+tar -xvzf relay-radar.0.10.03.tar.gz
+cd relay-radar
+pushd cil
+./configure
+make
+make check
+popd
+make
