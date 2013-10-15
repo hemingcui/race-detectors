7209dc77db4293387d17ee25b2afab7343243574
Xinan Xu <xx2153@columbia.edu>
2013-10-14 21:58:10 -0400

    full make script for both gcc and llvm

--- thread-sanitizer-2/mk.sh
+++ thread-sanitizer-2/mk.sh
@@ -2,11 +2,18 @@
 PREFIX=$HOME
 rm -rf llvm
 svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
-cd llvm
+pushd llvm
 R=$(svn info | grep Revision: | awk '{print $2}')
 (cd tools && svn co -r $R http://llvm.org/svn/llvm-project/cfe/trunk clang)
 (cd projects && svn co -r $R http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt)
 mkdir build
-cd build
+pushd build
 CC=gcc-4.5 CXX=g++-4.5 ../configure --prefix=$PREFIX --enable-assertions --enable-debug-runtime --enable-optimized
 make -j25 && make -j25 install || exit 1
+popd
+popd
+rm -rf gcc
+svn co svn://gcc.gnu.org/svn/gcc/trunk gcc
+cd gcc && mkdir build && cd build
+../configure --prefix=$PREFIX --enable-languages=c,c++ --disable-bootstrap --enable-checking=no
+make -j25 && make install -j25 || exit 1
