2e2127f61fd8d04f3164eccd58d9497df7c89e96
Xinan Xu <xx2153@columbia.edu>
2013-10-17 16:18:47 -0400

    Nits

--- mk-all.sh
+++ mk-all.sh
@@ -2,6 +2,12 @@
 set -e
 pushd thread-sanitizer
 ./mk.sh
-popd thread-sanitizer
+popd
+pushd thread-sanitizer-2
+./mk.sh
+popd
+pushd relay
+./mk.sh
+popd
 echo "please set the environment variable DATA_RACE_DETECTION_ROOT to `pwd`"
 echo "done!"
